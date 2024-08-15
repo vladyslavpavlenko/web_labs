c654c428651ac16b627af9b4724d9bdf45c6db06
Vladyslav Pavlenko <vladyslavpavlen@gmail.com>
2024-08-15 23:53:49 +0300

    feat: add TelegramUI

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '@telegram-apps/telegram-ui/dist/styles.css';
+import { AppRoot, Placeholder } from '@telegram-apps/telegram-ui';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <AppRoot>
+            <Placeholder
+                header="Title"
+                description="Description"
+            >
+                <img
+                    alt="Telegram sticker"
+                    src="https://xelene.me/telegram.gif"
+                    style={{ display: 'block', width: '144px', height: '144px' }}
+                />
+            </Placeholder>
+        </AppRoot>
     </>
   )
 }
